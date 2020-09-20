2e5fd08c74f7634a9e6c6009b15c4e970a430022
lalitkumar09 <55004095+lalitkumar09@users.noreply.github.com>
2020-09-20 20:05:55 +0200

    Create server.py

---
+++ server.py
@@ -0,0 +1,47 @@
+ 
+import socket 
+import threading  
+PORT = #PORT
+ 
+SERVER = socket.gethostbyname(socket.gethostname()) 
+ 
+ADDRESS = (SERVER, PORT) 
+FORMAT = "utf-8" 
+clients, names = [], [] 
+
+# Create a new socket for the server 
+server = socket.socket(socket.AF_INET,socket.SOCK_STREAM) 
+server.bind(ADDRESS) 
+ 
+def startChat(): 
+	print("server is working on " + SERVER) 
+	server.listen() 
+	
+	while True:  
+		conn, addr = server.accept() 
+		conn.send("NAME".encode(FORMAT))  
+		name = conn.recv(1024).decode(FORMAT) 
+		clients.append(conn) 
+		print(f"Name is :{name}")  
+		broadcastMessage(f"{name} has joined the chat!".encode(FORMAT)) 
+		conn.send('Connection successful!'.encode(FORMAT)) 
+		thread = threading.Thread(target = handle, args = (conn, addr)) 
+		thread.start() 
+		print(f"active connections {threading.activeCount()-1}") 
+
+def handle(conn, addr): 
+	print(f"new connection {addr}") 
+	connected = True
+	
+	while connected: 
+		message = conn.recv(1024) 
+		broadcastMessage(message) 
+	conn.close() 
+
+
+def broadcastMessage(message): 
+	for client in clients: 
+		client.send(message) 
+
+ 
+startChat() 
